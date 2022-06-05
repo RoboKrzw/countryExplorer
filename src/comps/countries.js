49112f45de7f8205d064a239d94c0f133c6d6736
RoboKrzw <r.krzywkowski@gmail.com>
2022-06-05 14:30:04 +0200

    changed how the buttons are constructed, from 2 elements to 1

--- src/comps/countries.js
+++ src/comps/countries.js
@@ -7,7 +7,6 @@ const countries = [
         {
           url: '/images/polska.jpg',
           title: 'Polska',
-          width: width,
           content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
         }
     },
@@ -17,7 +16,6 @@ const countries = [
         {
           url: '/images/francja.jpg',
           title: 'Francja',
-          width: width,
           content: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum."
         }
     },
@@ -27,7 +25,6 @@ const countries = [
         {
           url: '/images/slovakia.jpg',
           title: 'Slovakia',
-          width: width,
           content: "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000."
         }
     },
@@ -37,7 +34,6 @@ const countries = [
         {
           url: '/images/hungary.jpg',
           title: 'Hungary',
-          width: width,
           content: "Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words"
         }
     },
@@ -47,7 +43,6 @@ const countries = [
         {
           url: '/images/italia.jpg',
           title: 'Italia',
-          width: width,
           content: "The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33"
         }
     },
@@ -57,7 +52,6 @@ const countries = [
         {
           url: '/images/slovenia.jpg',
           title: 'Slovenia',
-          width: width,
           content: "200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable."
         }
     },
